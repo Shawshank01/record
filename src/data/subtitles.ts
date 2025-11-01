664fdbe32daebb0511b08e2fe3afcb33b057e69c
Devin <lelouch@outlook.ie>
2025-11-01 03:29:07 +0000

    Add subtitles

---
+++ src/data/subtitles.ts
@@ -0,0 +1,6 @@
+export const headerSubtitleOptions: string[] = [
+  "This blog is entirely produced by silicon-based slaves. I only issue commands and wield the whip.",
+  "Thanks to the AI era, I too have the opportunity to become a \"slave\" driver, whipping others to work for me. Or perhaps I've merely discovered an intensely addictive method of whipping myself?",
+  "I know nothing except the fact of my ignorance.\n\u2014 Socrates",
+  "Si vous ne vivez pas comme vous pensez, vous finirez par penser comme vous vivez.\n\u2014 Paul Bourget"
+];
