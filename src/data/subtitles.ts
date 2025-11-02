5babae8880fdb654f4b9984c758b21e6ea22366d
Devin <lelouch@outlook.ie>
2025-11-02 00:32:34 +0000

    Update subtitiles

--- src/data/subtitles.ts
+++ src/data/subtitles.ts
@@ -2,5 +2,12 @@ export const headerSubtitleOptions: string[] = [
   "This blog is entirely produced by silicon-based slaves. I only issue commands and wield the whip.",
   "Thanks to the AI era, I too have the opportunity to become a \"slave\" driver, whipping others to work for me. Or perhaps I've merely discovered an intensely addictive method of whipping myself?",
   "I know nothing except the fact of my ignorance.\n\u2014 Socrates",
-  "Si vous ne vivez pas comme vous pensez, vous finirez par penser comme vous vivez.\n\u2014 Paul Bourget"
+  "Si vous ne vivez pas comme vous pensez, vous finirez par penser comme vous vivez.\n\u2014 Paul Bourget",
+  "Unlimited tolerance must lead to the disappearance of tolerance.\n\u2014 Karl Popper",
+  "Freedom has many difficulties and democracy is not perfect, but we have never had to put a wall up to keep our people in, to prevent them from leaving us.\n\u2014 John F. Kennedy",
+  "The value of a college education is not the learning of many facts but the training of the mind to think.\n\u2014 Albert Einstein",
+  "Most people would rather die than think and many of them do!\n\u2014 Bertrand Russell",
+  "In the end the Party would announce that two and two made five, and you would have to believe it.\n\u2014 1984 by George Orwell",
+  "Three passions, simple but overwhelmingly strong, have governed my life: the longing for love, the search for knowledge, and unbearable pity for the suffering of mankind. These passions, like great winds, have blown me hither and thither, in a wayward course, over a deep ocean of anguish, reaching to the very verge of despair.\n\u2014 What I Have Lived For by Bertrand Russell",
+  "The unexamined life is not worth living.\n\u2014 Socrates"
 ];
